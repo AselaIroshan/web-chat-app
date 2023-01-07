93144dd4c6aa630b1361c705a0674351a153e944
iroshan <aselairoshan11@gmail.com>
2023-01-07 20:06:07 +0530

    retrive form data

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-
-ar express = require('express');
+const express = require('express');
 const path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var router = express.Router();
 var PORT =3000  
-// This middleware will not allow the
-// request to go beyond it
 
-    
-// Requests will never reach this route
+
 app.get('/', function (req , res) {
     res.sendFile(path.join(__dirname, '/index.html'))
-    
+});
+
+app.post('/',(req,res)=>{
+    username= req.body.u
+    password= req.body.p
+
 });
 
 
-app.post("/",function(req,res) {
-    console.log(req.body)
-})
-  
+
+
+
 app.listen(PORT, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
