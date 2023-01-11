898d492f3a04a721bf87a501b08d806fd54f260f
iroshan <aselairoshan11@gmail.com>
2023-01-11 12:28:50 +0530

    added chats

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 var PORT =3000  
 const chats = []
 
-
+// loging sever,req
 app.get('/', function (req , res) {
     res.sendFile(path.join(__dirname, '/index.html'))
 });
 
+// loging server, res 
 app.post('/',(req,res)=>{
     let username= req.body.u
     let password= req.body.p
@@ -22,23 +23,25 @@ app.post('/',(req,res)=>{
 });
 
 
-
+// fpage sever,req
 app.get("/fpage",function(req,res) {
 
     res.render("page/fpage",{chat:chats
     });
 });
 
+// fpage sever,res
 app.post("/fpage",function(req,res){
-    let chat= req.body.chat
-    chats.push({name:chat})
+    let data = req.body
+    chats.push(data)
+    console.log(chats)
 
 });
 
 
 
 
-
+//lissing port 3000
 app.listen(PORT, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
--- public/script/fpage.js
+++ public/script/fpage.js
@@ -1,1 +1,9 @@
-$("form")
+$("button").click(function (e) { 
+    e.preventDefault();
+    let chat = $("input").val();
+    $.post( "/fpage", { name:chat },function(err){
+        if (err){
+            console.log(err)
+        }
+    } );
+});
